fc9394048316583c8d27f635fa46590042539b9a
yauyuirate <temple9well@gmail.com>
2017-01-09 12:25:07 +0900

    Add username field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105234311) do
+ActiveRecord::Schema.define(version: 20170109014356) do
 
   create_table "pons", force: :cascade do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170105234311) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
