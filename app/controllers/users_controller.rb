a0cf8faa9480a8303b633a932918fdc576539d9c
yauyuirate <temple9well@gmail.com>
2017-01-16 20:09:43 +0900

    Create link user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find_by username: params[:username]  
+  end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :pons
   devise_for :users
+
+  get ':username' => 'users#show', as: 'user'
+
   root 'home#index'
 
 end
