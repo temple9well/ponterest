259d7f16cc1813f66343ed903b810da945d81028
yauyuirate <temple9well@gmail.com>
2017-01-19 18:24:38 +0900

    Add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :pons
   validates_uniqueness_of :username
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
